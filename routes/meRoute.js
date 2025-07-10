d7de1c3df421e9a2a73dd10b603241c495a49439
Emon Mahmud <emonmahmud083@gmail.com>
2025-07-11 02:56:17 +0600

    add me api

--- routes/meRoute.js
+++ routes/meRoute.js
@@ -2,13 +2,19 @@ const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-
-
 router.get("/me", authMiddleware, (req, res) => {
-  res.status(200).json({
-    message: "User data retrieved successfully",
-    user: req.user,
-  });
+try{
+    console.log(req.user);
+  if(!req.user){
+    return res.status(403).json({message:"unauthorized access"})
+  }
+    res.status(200).json({
+      message: "User data retrieved successfully",
+      user: req.user,
+    });
+}catch(error){
+    return res.status(500).json({message:"internal server error"})
+}
 });
 
 module.exports = router;
